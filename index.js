0bd56d2ce45604a86d6ce24bc02780d0d9d45489
unknown <okwuosahpashal@gmail.com>
2021-08-04 14:25:32 +0100

    anoth  commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();

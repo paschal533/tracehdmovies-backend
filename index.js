4c491d486caf566f8732b839cd2fb15ebc805d09
unknown <okwuosahpashal@gmail.com>
2021-08-04 15:01:37 +0100

    make t bt

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
   });
-
+app.disable('etag');
 app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin 
